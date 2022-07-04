b53f5f0765f4adce5253aadbf8a61b05b5559165
Khushi Panwar <102166679+smilewithkhushi@users.noreply.github.com>
2022-07-04 17:46:13 +0530

    Create String & PinkBG using AWT.java

---
+++ AWT & Swing/String & PinkBG using AWT.java
@@ -0,0 +1,31 @@
+//9.	Using AWT, write a program to display a string in frame window with pink color as background.
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class StringBackground extends Frame{
+	Dimension o1;
+	
+	public StringBackground() {
+		setSize(800,800);
+		setBackground(Color.pink);
+		addWindowListener(new MyWindowAdapter());
+	}
+	
+	public void paint(Graphics g) {
+		g.drawString("Hello World!", 300 , 300);
+	}
+	
+	public static void main(String[] args) {
+		StringBackground o1 = new StringBackground();
+		o1.setTitle("QUESTION 9");
+		o1.setVisible(true);
+	}
+
+	class MyWindowAdapter extends WindowAdapter{
+	public void windowClosing(WindowEvent o1) {
+		System.exit(0);
+	}
+}
+}
+
