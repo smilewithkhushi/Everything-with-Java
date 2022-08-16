aac370eec00a7ceb77e05cb97f19e632ea994cd2
Khushi Panwar <102166679+smilewithkhushi@users.noreply.github.com>
2022-08-16 21:01:11 +0530

    Update 1(b)_ProductOfDigits.java

--- Exam_Solution/1(b)_ProductOfDigits.java
+++ Exam_Solution/1(b)_ProductOfDigits.java
@@ -1,1 +1,22 @@
 
+import java.io.*;
+
+public class Solution1_b {
+	
+	public static int recMeth(int n) {
+		if (n==1) {
+			return 1;
+		}
+		return (n%10 * recMeth(n/10));
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int num,result;
+		System.out.print("Enter the number : ");
+		num=Integer.parseInt(br.readLine());
+		result=recMeth(num);
+		System.out.println("\n -> The product of digits of "+num+" is "+result);	
+		
+	}
+
+}
