aa4251f4e97e5a8807f49d5c216dcd9f89cd0697
Khushi Panwar <102166679+smilewithkhushi@users.noreply.github.com>
2022-08-16 21:15:07 +0530

    Update 1(d)_ObjReturn.java

--- Exam_Solution/1(d)_ObjReturn.java
+++ Exam_Solution/1(d)_ObjReturn.java
@@ -1,1 +1,30 @@
+import java.io.*;
+import java.math.*;
+class ObjReturn{
+	Float a;
+	
+	ObjReturn(float n){
+		a=n;
+	}
+	
+	public ObjReturn decOb() {
+		int temp=Math.round(a);
+		float diff=(float) (a-temp);
+		System.out.println(diff);
+		ObjReturn o2=new ObjReturn(diff);
+		return o2;
+	}
+}
+
+public class Solution1_c {
+	
+	public static void main(String[] args)throws IOException{
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	Float num;
+	System.out.println("Enter the floating point number : ");
+	num=Float.parseFloat(br.readLine());
+	ObjReturn o1=new ObjReturn(num);
+	o1.decOb();
+}
+}
 
